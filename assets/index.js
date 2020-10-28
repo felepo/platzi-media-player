2ddbb2b1b680bab437cedceecc41bce820549a9c
Fernando Poncio <fecrackle@gmail.com>
2020-10-27 23:25:48 -0600

    add AutoPlay plugin and mute/unmute function

--- assets/index.js
+++ assets/index.js
@@ -1,7 +1,14 @@
 import MediaPlayer from './MediaPlayer.js'
+import AutoPlay from './plugins/AutoPlay.js'
 
 const video = document.querySelector('video')
-const button = document.querySelector('button')
+const buttonPlay = document.querySelector('.btn-play')
+const buttonMute = document.querySelector('.btn-mute')
 
-const player = new MediaPlayer({ el: video })
-button.onclick = () => player.togglePlay()
+const player = new MediaPlayer({
+  el: video,
+  plugins: [new AutoPlay()],
+})
+
+buttonPlay.onclick = () => player.togglePlay()
+buttonMute.onclick = () => player.toggleMute()
